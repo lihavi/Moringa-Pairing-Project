cbb59db656bd26889103c9625496632487a2aa71
kevinkkimutai <kiruikevinkim@gmail.com>
2023-04-18 19:58:49 +0300

    navbar logo added and styled

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 
 function Homepage() {
   return (
@@ -7,8 +6,7 @@ function Homepage() {
       <div className="home-banner">
         <h1>Moringa Pair</h1>
         <p>  Pairs students randomly and keeps track of each pair.</p>
-        <Link to="/loginform" className="btn btn-primary">Login</Link>
-          <Link to="/signupform" className="btn btn-primary">Sign up</Link>
+       
       </div>
       </div>
   );
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,28 +1,24 @@
+import axios from "axios";
 import {useState} from "react";
 
-function LoginForm({ onLogin }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      const response = await fetch('/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
+function LoginForm({ setToken }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(`http://localhost:3000/users/login`, {
+        email,
+        password,
       });
-  
-      if (response.ok) {
-        const data = await response.json();
-        onLogin(data.session_token);
-      } else {
-        const error = await response.json();
-        console.error(error);
-      }
-    };
+      localStorage.setItem("token", response.data.token);
+      setToken(response.data.token);
+      console.log(response.data.user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   
     return (
       <form onSubmit={handleSubmit}>

f4dbcd8bcfd5d0a3d4e80a4cfad6f2a6ce7aeaad
Mickey254-ja <mickey.awino@student.moringaschool.com>
2023-04-19 15:31:39 +0300

    creates tests

--- api/app/models/student.rb
+++ api/app/models/student.rb
@@ -7,19 +7,4 @@ class Student < ApplicationRecord
   validates :fullname, presence: true
   validates :grade, presence: true
 
-  # # Define a method to generate pairs of students
-  # def self.generate_pairs
-  #   # Get and reshuffle students from the database
-  #   students = self.all.shuffle
-  #   # Initialize an empty array
-  #   pairs = []
-  #   # Iterate over the students array two at a time
-  #   students.each_slice(2) do |student1, student2|
-  #     # Create a new pair object and add it to the pairs array
-  #     pairs << Pair.create(student1: student1, student2: student2)
-  #   end
-  #   # Return the pairs array
-  #   pairs
-  # end
-
 end

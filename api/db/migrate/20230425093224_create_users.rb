85f5feb807c9f1fc78ed4d1a3dc69bfd1ca9cd5f
vivianne <viviannjoroge91@gmail.com>
2023-04-27 13:47:14 +0300

    add changes

--- api/db/migrate/20230425093224_create_users.rb
+++ api/db/migrate/20230425093224_create_users.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
+    unless table_exists? :users
     create_table :users do |t|
       t.string "fullname"
       t.string "email"
@@ -10,4 +11,5 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.timestamps
     end
   end
+end
 end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -15,25 +15,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_25_093224) do
   enable_extension "plpgsql"
 
   create_table "feedbacks", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "instructors", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "fullname"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.integer "sender_user_id"
-    t.integer "recipient_user_id"
-    t.string "content"
     t.integer "pair_id"
+    t.integer "instructor_id"
+    t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

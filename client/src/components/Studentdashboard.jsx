45292abce2a171488f9da96b2ff3384cddfe1f2a
kevinkkimutai <kiruikevinkim@gmail.com>
2023-04-28 14:51:24 +0300

    dashboard styled

--- client/src/components/Studentdashboard.jsx
+++ client/src/components/Studentdashboard.jsx
@@ -22,40 +22,42 @@ function Studentdashboard({token}) {
   return (
  
    
-      <div className="container  pt-5 me-5 admindashboard">
-  <div className="row align-items-center ms-5 pt-5">
-    <div className="col">
-     <div className="card bg-dark" style={{width: "18rem"}}>
+      <div className="containe pt-5 me-5 admindashboar">
+          <h3 className="stdash">Students Dashboard</h3>
+<div class="row justify-content-center pt-4">
+  
+    <div class="col-3">
+   <div className="row pt-3">
+   <div className="card bg-dark " style={{width: "18rem"}}>
   <div className="card-body">
     <h4 className="card-title ms-0"><i className="fas fa-users fa-fw me-3"></i><span>Group</span></h4>
-   <h6 className="total">No: no  <a className="ms-4" href="/"> view</a> </h6>
+   <h6 className="total ms-5">No: no  <a className="ms-4" href="/"> view</a> </h6>
   
   </div>
 </div>
-    </div>
-    <div className="col">
-    <div className="card bg-dark" style={{width: "18rem"}}>
-  <div className="card-body">
-    <h4 className="card-title ms-0"><i className="fas fa-users fa-fw me-3"></i><span>Grade</span></h4>
-    <h6 className="total">grade: 10  <a className="ms-4" href="/"> view</a> </h6>
-  </div>
-</div>
-    </div>
-    <div className="col">
-    <div className="card bg-dark" style={{width: "18rem"}}>
-  <div className="card-body">
-    <h4 className="card-title ms-0"><i className="fas fa-users fa-fw me-3"></i><span>Assigned TM</span></h4>
-    <h6 className="total">Name: Name of TM</h6>
-  </div>
-</div>
-    </div>
-  </div>
+   </div>
 
-  <div class="row justify-content-center pt-5 ms-5">
-    <div class="col-5 text-start">
-    <h3><u>Strenghts</u></h3>
-    {user &&
-      <div className="car cardp">
+<div className="row pt-3">
+<div className="card bg-dark pt-2" style={{width: "18rem"}}>
+  <div className="card-body">
+    <h4 className="card-title ms-0"><i class="fas fa-graduation-cap fa-fw me-3"></i><span>Grade</span></h4>
+    <h6 className="total ms-5">grade: 10  <a className="ms-4" href="/"> view</a> </h6>
+  </div>
+</div>
+</div>
+
+<div className="row pt-3">
+<div className="card bg-dark" style={{width: "18rem"}}>
+  <div className="card-body">
+    <h4 className="card-title ms-0"><i className="fas fa-user fa-fw me-3"></i><span>Assigned TM</span></h4>
+    <h6 className="total ms-5">Name: Name of TM</h6>
+  </div>
+</div>
+</div>
+    </div>
+    <div class="col-4">
+     {user &&
+      <div className="card cardp">
         <div className="upper">
           {/* <img src={img} className="img-fluid" height="5" alt='img' />*/}
         </div>
@@ -64,16 +66,16 @@ function Studentdashboard({token}) {
             <img src={img} className="rounded-circle" width="60" alt="img" />
           </div>
         </div>
-        <div className="mt-5 text-center">
-          <h4 className="mb-0">{user.fullname}</h4>
+        <div className="mt-4 text-center">
+          <h5 className="mb-0">Username: {user.fullname}</h5>
           <div className="d-flex justify-content-between align-items-center mt-4 ms-5 px-4">
             <div className="stats">
               <h6 className="mb-0">Role</h6>
-              <span>{user.role}</span>
+              <h5>{user.role}</h5>
             </div>
             <div className="stats">
               <h6 className="mb-0">Email</h6>
-             <span>{user.email}</span>
+             <h5>{user.email}</h5>
             </div>
           </div>
           <div className="d-flex pbtn ">
@@ -85,10 +87,7 @@ function Studentdashboard({token}) {
       </div>
     }
     </div>
-    
   </div>
-
-
 </div>
   
    
--- client/src/components/profile/Adminprofile.jsx
+++ client/src/components/profile/Adminprofile.jsx
@@ -4,7 +4,8 @@ import img from '../../assets/logo.png'
 
 
 const Adminprofile = ({token}) => { 
-   const [user, setUser] = useState(null);
+   
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:3000/user/me', {
@@ -21,10 +22,53 @@ const Adminprofile = ({token}) => {
         console.error(error);
       });
   }, [token]);
-
   return (
-    <div className="containr d-flex justify-content-center align-items-center pt-5">
-        {user &&
+ 
+   
+      <div className="containe pt-5 me-5 admindashboar">
+          <h3 className="stdash">Admin Dashboard</h3>
+<div class="row justify-content-center pt-3">
+  
+    <div class="col-3">
+   <div className="row pt-2">
+   <div className="card bg-dark " style={{width: "18rem"}}>
+  <div className="card-body">
+    <h4 className="card-title ms-0"><i className="fas fa-layer-group me-3"></i><span>Groups</span></h4>
+   <h6 className="total ms-5">No: no  <a className="ms-4" href="/"> view</a> </h6>
+  
+  </div>
+</div>
+   </div>
+
+<div className="row pt-2">
+<div className="card bg-dark pt-2" style={{width: "18rem"}}>
+  <div className="card-body">
+    <h4 className="card-title ms-0"><i class="fas fa-message fa-fw me-3"></i><span>FeedBacks</span></h4>
+    <h6 className="total ms-5">grade: 10  <a className="ms-4" href="/"> view</a> </h6>
+  </div>
+</div>
+</div>
+
+<div className="row pt-2">
+<div className="card bg-dark" style={{width: "18rem"}}>
+  <div className="card-body">
+    <h4 className="card-title ms-0"><i className="fas fa-users fa-fw me-3"></i><span>Students</span></h4>
+    <h6 className="total ms-5">Name: Name of TM</h6>
+  </div>
+</div>
+</div>
+
+<div className="row pt-2">
+<div className="card bg-dark" style={{width: "18rem"}}>
+  <div className="card-body">
+    <h4 className="card-title ms-0"><i className="fas fa-list-ol fa-fw me-3"></i><span>Pairs</span></h4>
+    <h6 className="total ms-5">Name: Name of TM</h6>
+  </div>
+</div>
+</div>
+    </div>
+    <div class="col-4 pt-4">
+     {user &&
       <div className="card cardp">
         <div className="upper">
           {/* <img src={img} className="img-fluid" height="5" alt='img' />*/}
@@ -34,16 +78,16 @@ const Adminprofile = ({token}) => {
             <img src={img} className="rounded-circle" width="60" alt="img" />
           </div>
         </div>
-        <div className="mt-5 text-center">
-          <h4 className="mb-0">Name: {user.fullname}</h4>
+        <div className="mt-4 text-center">
+          <h5 className="mb-0">Username: {user.fullname}</h5>
           <div className="d-flex justify-content-between align-items-center mt-4 ms-5 px-4">
             <div className="stats">
               <h6 className="mb-0">Role</h6>
-              <span>{user.role}</span>
+              <h5>{user.role}</h5>
             </div>
             <div className="stats">
               <h6 className="mb-0">Email</h6>
-             <span>{user.email}</span>
+             <h5>{user.email}</h5>
             </div>
           </div>
           <div className="d-flex pbtn ">
@@ -55,6 +99,10 @@ const Adminprofile = ({token}) => {
       </div>
     }
     </div>
+  </div>
+</div>
+  
+   
   );
 }
 
